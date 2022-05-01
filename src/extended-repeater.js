7151aa389db047eabf84a39007e775cad8547326
lordb <bladov123@gmail.com>
2022-05-01 16:09:58 +0300

    feat: add extened task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,35 +15,60 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
-
-// function repeater(str, options) {
-//   let result = [`${str}`];
-//   let addition = [`${options.addition}`];
-
-//   options.separator ? options.separator : options.separator = '+'
-//   options.additionSeparator ? options.additionSeparator : options.additionSeparator = '|';
-
-
-//   for (let i = 0; i < options.additionRepeatTimes; i++){
-//       addition[i] = `${options.addition}`;
-//   }
-
-//   for (let i = 0; i < options.repeatTimes; i++){
-//       if (!options.hasOwnProperty(addition)) {
-//           result[i] = str;
-//           continue;
-//       }
-//       result[i] = str + (addition.join(options.additionSeparator) || '');
-//   }
-
-
-//   return result.join(options.separator)
-
+// function repeater(/* str, options */) {
+//   throw new NotImplementedError('Not implemented');
+//   // remove line with error and write your code here
 // }
+
+function repeater(str, options) {
+  let string = `${str}`;
+  let additionArr;
+  let result = [];
+
+
+   if (!options.hasOwnProperty('separator')) {
+       options.separator = '+';
+   }
+
+   if (!options.hasOwnProperty('additionSeparator')) {
+       options.additionSeparator = '|';
+   }
+
+   //merge down
+  //  if (options.hasOwnProperty('addition')) {
+  //      options.addition = `${options.addition}`;
+  //  }
+
+   if(options.hasOwnProperty('addition')) {
+       options.addition = `${options.addition}`;
+
+       if(options.hasOwnProperty('additionRepeatTimes')) {
+           additionArr = [];
+           for (let i = 0; i < options.additionRepeatTimes; i++) {
+               additionArr[i] = options.addition;
+           }
+
+           additionArr = additionArr.join(options.additionSeparator)
+       } else {
+           additionArr = options.addition
+       }
+   }
+
+   if (options.hasOwnProperty('repeatTimes')) {
+       for (let i = 0; i < options.repeatTimes; i++) {
+           if (additionArr) {
+               result[i] = string + additionArr;
+               continue;
+           }
+
+           result[i] = string;
+       }
+   } else {
+        result[0] = string + additionArr || '';
+    }
+
+  return result.join(options.separator)
+}
 
 module.exports = {
   repeater
